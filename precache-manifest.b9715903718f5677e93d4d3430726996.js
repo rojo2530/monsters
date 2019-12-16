256d17f616ad2887f5ef3f6273de137544ad5132
Jose <jcantosg@gmail.com>
2019-12-16 13:33:49 +0100

    Updates

---
+++ precache-manifest.b9715903718f5677e93d4d3430726996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160009ca6e346242b2825dca524d418d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e337e0cf6bc7e8237d8d",
+    "url": "/monsters/static/css/main.3886787b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e337e0cf6bc7e8237d8d",
+    "url": "/monsters/static/js/main.d2436871.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
